9fc97248c4110352a2d9baa0bc7e44393167d3d0
thomasece23 <61596257+thomasece23@users.noreply.github.com>
2021-03-26 16:10:53 +0100

    Css sur page SellerRegister + Ajout sellerRegister.css
    ta catpé

--- sellerRegister.php
+++ sellerRegister.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
 <link rel="icon" type="image/png" href="icon.png"/>
-<link rel="stylesheet" href="index.css">
+<link rel="stylesheet" href="sellerRegister.css">
 
 
 	
@@ -18,6 +18,29 @@
 </head>
 
 <body>
+
+<div id="tlois">
+	 &emsp;CIGAR SHOP SINCE 1955 
+
+</div>	
+
+
+<div id="title"> 
+	Cigar Shop
+</div>
+
+<section>
+<div class="wrapper">
+    <ul>
+	<li><a href="index.php">Home</li>
+<li> Categories
+            <ul>
+<li><a href="cigars.php">Cigars</a></li>
+<li><a href="">Accessories</li></li>
+</ul><li> <a href="sellObject.php">Sell </a></li>
+<li><a href="">Your account </a></li></ul>
+<a href="cart.php"><i class="fas fa-shopping-cart"></i></a>
+</div><br><br>
 
 <?php
 $profilFound = 0;
@@ -59,36 +82,47 @@ $profilFound = 2;
 ?>
 
 
+
 <form name = "inputForm" action = "" method = "post">
-		<legend>PERSONAL INFORMATIONS</legend>
-			<table border="1">
-			<tr>
-			<td>	First Name : </td>
-			<td>	<input type="text" id="firstname" name="firstname"> </td>
-
-				<td>Mail :</td>
-				<td><input type="text" id="mail" name="mail"> </td>
-	</tr>
-	<tr>
-		<td>Last Name :</td>
-			<td>	<input type="text" id="lastname" name="lastname"></td>
-
-			<td>Password:</td>
+	<div id="Register_form">
+	<br>	
+	<h3>PERSONAL INFORMATIONS</h3>
 			
-			<td>	<input type="password" id="password" name="password"> </td>	
-	      </td>
-			</tr>
-			</table>
+				 
+				<input type="text" class="infos" id="firstname" name="firstname" placeholder="First name"> <br>
+				<input type="text" class="infos" id="lastname" name="lastname" placeholder="Last name"><br>
+				
+				<input type="text"  class="infos" id="mail" name="mail" placeholder="Mail"> <br>
 
+				<input type="password" class="infos" id="password" name="password" placeholder="Password"> <br>
+				
 		
-			<br> <br><br> <br><br> <br>
-			<input type = "submit"  value = "submit" name = "submit"/>
-</form>
+			<br> 
+			<input type = "submit" class="bouton"  value = "SUBMIT" name = "submit"/>
+			
+			
+
+	</div>
+
+</form></section>
 		
-		
-		
-			<button onclick="reset()">Reset</button>
-		
+					
+<div id="bottom"> 
+ <br>CONTACT<br><br>
+ Opus Management S.A.<br>
+Impasse de Champ Colin N°6<br>
+1260 Nyon - Suisse <br>
+Hotline +41 (0)79 104 19 98<br>
+Email info@cigarshop.com <br>
+<br>
+<span id="social"> <img src="socialnetwork.png"></span>
+
+</div>
+
+<div id="credits"> 
+<div id="copyright">Copyright 2021 © Cigar Shop - Opus Management S.A.</div>
+<span id="payments"> <img src="payments.png"></span>
+</div>
 		
 		
  </body>
